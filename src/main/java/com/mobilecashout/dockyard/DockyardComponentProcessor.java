b87101b841969cfbcd7a4d9c34029f9be5a24491
Matīss Treinis <mrtreinis@gmail.com>
2019-10-22 18:33:10 +0200

    Bump versions

--- src/main/java/com/mobilecashout/dockyard/DockyardComponentProcessor.java
+++ src/main/java/com/mobilecashout/dockyard/DockyardComponentProcessor.java
@@ -2,6 +2,7 @@ package com.mobilecashout.dockyard;
 
 import com.google.auto.common.BasicAnnotationProcessor;
 
+import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.tools.JavaFileObject;
@@ -11,10 +12,17 @@ import java.util.HashMap;
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class DockyardComponentProcessor extends BasicAnnotationProcessor {
     final static HashMap<String, JavaFileObject> GENERATED_SOURCES = new HashMap<>();
+
     @Override
     protected Iterable<? extends ProcessingStep> initSteps() {
         return Collections.singletonList(
                 new DockyardPickupStep(processingEnv)
         );
+    }
+
+    @Override
+    protected void postRound(RoundEnvironment roundEnv) {
+        super.postRound(roundEnv);
+        GENERATED_SOURCES.clear();
     }
 }
